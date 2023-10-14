a8107d8fd901e6c1ea1225a2b68b253b338eb26e
KuganVlad <vlad_kugan@hotmail.com>
2023-10-14 21:17:12 +0300

    Initial Commit

---
+++ To-Do-List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do-List
+//
+//  Created by Vlad Kugan on 14.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
